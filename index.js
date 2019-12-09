95f5a51e251af716b11a2a1c150fba521dcaae35
eaglecrazy <eaglecrazy@mail.ru>
2019-12-09 20:05:01 +0300

    Поправил стилизацию, сделал рефакторинг функций

--- index.js
+++ index.js
@@ -10,15 +10,14 @@ const goods = [
     {title: 'Contra', price: 140}
 ];
 
-const renderGoodsItem = (title, price) => {
-    return `<div class="goods-item"><h3>${title}</h3><img class="item-image" src="img/game.jpg" width="250" height="156" alt="${title}"><p>Цена: ${price} рублей</p><button class="button">Добавить в корзину</button></div>`;
-};
+const renderGoodsItem = (title = 'Название', price = 0) => 
+    `<div class="goods-item"><h3 class="goods-item-heading">${title}</h3><img class="item-image" src="img/game.jpg" width="250" height="156" alt="${title}"><p class="goods-item-text">Цена: ${price} рублей</p><button class="button">Добавить в корзину</button></div>`;
+;
 
 
-const renderGoodsList = (list) => {
+const renderGoodsList = (list = []) => {
     const goodsList = list.map(item => renderGoodsItem(item.title, item.price));
-    let str = (goodsList.toString()).replace(/,/g, '');
-    document.querySelector('.goods-list').innerHTML = str;
+    document.querySelector('.goods-list').innerHTML = goodsList.join('');
 };
 
 
