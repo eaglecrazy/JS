bf8cd7f4e574a66dd3239875fae819de632527a6
eaglecrazy <eaglecrazy@mail.ru>
2019-12-10 20:50:29 +0300

    Делал ДЗ №2

--- index.js
+++ index.js
@@ -41,6 +41,47 @@ class GoodsList {
     }
 }
 
+class Basket {
+    constructor() {
+        this.goods = [];
+    }
+    addItem(item) {
+        this.goods.push(item);
+    }
+    removeItem(itemTitle) {
+        let i;
+        for(i = 0; i < this.goods.length; i++) {
+            if(this.goods[i].title === itemTitle) {
+                break;
+            }
+        }
+        if (i !== undefined) {
+            this.goods.splice(i, 1);
+        }
+    }
+    costAllItems() {
+        let cost = 0;
+        this.goods.forEach(good => {
+            cost += good.price;        
+        });
+        return cost;
+    }   
+}
+
+/*
+1. Добавил класс "корзина". В этом классе нужны методы: конструктор, добавление товара, удаление товара и вывод полной стоимости всех товаров. Для элемента корзины товаров новая сущность пока не нужна, можно вполне использовать класс GoodsItem.
+
+2. Метод, определяющий суммарную стоимость всех товаров гораздо более актуален для корзины, поэтому я добавил его туда. Метод "costAllItems"
+*/
+
 const l = new GoodsList();
 l.fetchGoods();
 l.render();
+
+const b = new Basket();
+b.addItem(l.goods[2]);
+b.addItem(l.goods[3]);
+console.log(b.goods);
+console.log(b.costAllItems());
+b.removeItem(l.goods[2].title);
+console.log(b.goods);
