4ef1277d97ada385af754de078ff8dd8de113ee2
eaglecrazy <eaglecrazy@mail.ru>
2019-12-13 17:17:18 +0300

    Перевёл рендеринг чата в JS. Сделал классы сообщения и пользователя. Использовал reduce вместо foreach в корзине.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-/* jslint white: true */
-/*eslint-env browser*/
-/*eslint no-console: 0*/
+//1. Переделайте makeGETRequest() так, чтобы она использовала промисы.
+//2. Добавьте в соответствующие классы методы добавления товара в корзину, удаления товара из корзины и получения списка товаров корзины.
+//3* Переделайте GoodsList так, чтобы fetchGoods() возвращал промис, а render() вызывался в обработчике этого промиса.
 
 class GoodsItem {
     constructor(title = 'Без имени', price = 0) {
@@ -8,7 +8,12 @@ class GoodsItem {
         this.price = price;
     }
     render() {
-        return `<div class="goods-item"><h3 class="goods-item-heading">${this.title}</h3><img class="item-image" src="img/game.jpg" width="250" height="156" alt="${this.title}"><p class="goods-item-text">Цена: ${this.price} рублей</p><button class="button">Добавить в корзину</button></div>`;
+        return `<div class="goods-item">
+                    <h3 class="goods-item-heading">${this.title}</h3>
+                    <img class="item-image" src="img/game.jpg" width="250" height="156" alt="${this.title}">
+                    <p class="goods-item-text">Цена: ${this.price} рублей</p>
+                    <button class="button">Добавить в корзину</button>
+                </div>`;
     }
 }
 
@@ -18,17 +23,17 @@ class GoodsList {
     }
     fetchGoods() {
         this.goods = [{
-                title: 'Mario',
-                price: 150
+            title: 'Mario',
+            price: 150
         }, {
-                title: 'Castlevania',
-                price: 200
+            title: 'Castlevania',
+            price: 200
         }, {
-                title: 'Battletoads',
-                price: 180
+            title: 'Battletoads',
+            price: 180
         }, {
-                title: 'Contra',
-                price: 140
+            title: 'Contra',
+            price: 140
         }];
     }
     render() {
@@ -41,7 +46,7 @@ class GoodsList {
     }
 }
 
-class Basket {
+class Cart {
     constructor() {
         this.goods = [];
     }
@@ -50,8 +55,8 @@ class Basket {
     }
     removeItem(itemTitle) {
         let i;
-        for(i = 0; i < this.goods.length; i++) {
-            if(this.goods[i].title === itemTitle) {
+        for (i = 0; i < this.goods.length; i++) {
+            if (this.goods[i].title === itemTitle) {
                 break;
             }
         }
@@ -59,13 +64,18 @@ class Basket {
             this.goods.splice(i, 1);
         }
     }
-    costAllItems() {
-        let cost = 0;
-        this.goods.forEach(good => {
-            cost += good.price;        
-        });
-        return cost;
-    }   
+    totalPrice(){
+        return this.goods.reduce((sum, item) => {
+            if(item.price){
+                sum += item.price; 
+            }
+            return sum;
+        }, 0);
+        
+    }
+    clean() {
+        this.goods = [];
+    }
 }
 
 /*
@@ -78,10 +88,10 @@ const l = new GoodsList();
 l.fetchGoods();
 l.render();
 
-const b = new Basket();
+const b = new Cart();
 b.addItem(l.goods[2]);
 b.addItem(l.goods[3]);
 console.log(b.goods);
-console.log(b.costAllItems());
+console.log(b.totalPrice());
 b.removeItem(l.goods[2].title);
 console.log(b.goods);
